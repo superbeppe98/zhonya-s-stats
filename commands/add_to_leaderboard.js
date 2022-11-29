a0633863d6231ffe300d45129eace0b7a1970ac2
SuperBeppe98 <giuseppebrusco98@gmail.com>
2022-11-29 22:07:16 +0100

    fix underscore commands

--- commands/add_to_leaderboard.js
+++ commands/add_to_leaderboard.js
@@ -1,19 +1,29 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 const data = new SlashCommandBuilder()
-    .setName('add_to_leaderboard')
+    .setName('add')
     .setDescription('Add an user to the server leaderboard')
-    .addStringOption(option =>
-        option.setName('discord-username')
-            .setDescription('Discord username')
-            .setRequired(true))
-    .addStringOption(option =>
-        option.setName('summoner-name')
-            .setDescription('Summoner name')
-            .setRequired(true))
-    .addStringOption(option =>
-        option.setName('region')
-            .setDescription('Enter the region you want to use (ex: EUW for Europe West)')
-            .setRequired(true))
+    .addSubcommandGroup((group) =>
+        group
+            .setName('to')
+            .setDescription('Add an user to the server leaderboard')
+            .addSubcommand((subcommand) =>
+                subcommand
+                    .setName('leaderboard')
+                    .setDescription('Add an user to the server leaderboard')
+                    .addStringOption(option =>
+                        option.setName('discord-username')
+                            .setDescription('Discord username')
+                            .setRequired(true))
+                    .addStringOption(option =>
+                        option.setName('summoner-name')
+                            .setDescription('Summoner name')
+                            .setRequired(true))
+                    .addStringOption(option =>
+                        option.setName('region')
+                            .setDescription('Enter the region you want to use (ex: EUW for Europe West)')
+                            .setRequired(true))
+            )
+    )
 
 module.exports = { data }
--- commands/pino.js
+++ commands/pino.js
@@ -3,5 +3,4 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const data = new SlashCommandBuilder()
 	.setName('pino')
 	.setDescription('Replies with a secret phrase! *_*')
-
 module.exports = { data }
