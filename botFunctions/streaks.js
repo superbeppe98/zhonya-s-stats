b8cadfdb792e82f69814b5fac1aae494dfdc56b4
SuperBeppe98 <giuseppebrusco98@gmail.com>
2022-12-03 17:17:20 +0100

    fix .default

--- botFunctions/streaks.js
+++ botFunctions/streaks.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer").default;
+const puppeteer = require("puppeteer");
 const { EmbedBuilder } = require('discord.js');
 
 // Keep Puppeteer open so we don't have to open it each time we get a /streaks
